3318d729e4e7a7e62e2a1cb3c615f0694a42a678
ahmedcomaks <150958895+ahmedcomaks@users.noreply.github.com>
2024-02-09 07:57:14 +0200

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,50 @@
+const Orders = [
+    {
+        productName: 'foldable Mini Drone',
+        productNumber:'85698',
+        paymentStatus: 'Due',
+        shipping: 'pending',
+    },
+    {
+        productName: 'laevender kf102 drone',
+        productNumber:'15488',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined',
+    },
+    {
+        productName: 'Ruco f11 pro Drone',
+        productNumber:'54987',
+        paymentStatus: 'Due',
+        shipping: 'pending',
+    },
+    {
+        productName: 'Drone With Camera Drone',
+        productNumber:'51984',
+        paymentStatus: 'paid',
+        shipping: 'Deliverd',
+    },
+    {
+        productName: 'Gps 4k Drone',
+        productNumber:'64846',
+        paymentStatus: 'paid',
+        shipping: 'Deliverd',
+    },
+    {
+        productName: 'gps f12 Drone',
+        productNumber:'64894',
+        paymentStatus: 'Due',
+        shipping: 'pending',
+    },
+    {
+        productName: '4k camera Drone',
+        productNumber:'548115',
+        paymentStatus: 'Due',
+        shipping: 'pending',
+    },
+    {
+        productName: 'Drone With Camera',
+        productNumber:'26548',
+        paymentStatus: 'paid',
+        shipping: 'Deliverd',
+    },
+]
